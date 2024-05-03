a2de316d25513ef9bff4d4720cf14f8abf1efc56
MRBOX <olamigokeadebayo90@gmail.com>
2024-05-03 14:02:20 +0100

    finishing the frontend

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def signup():
+    return render_template("signup.html")
+
+
+
+
+app.run(debug=True, port=5001)
